f9c1eb9f5ace50509e14e7beb6641798ef046e05
yeswanthrepaka <yeswanthrepaka@gmail.com>
2026-02-05 12:10:05 +0530

    changes done

--- users.sh
+++ users.sh
@@ -8,6 +8,7 @@ G="\e[32m"
 Y="\e[33m"
 B="\e[34m"
 N="\e[0m"
+SCRIPT_NAME=$PWD
 
 if [ $USERID -ne 0 ]; then
     echo -e"$R Please run the script with root user $N" | tee -a $LOGS_FILE
@@ -21,7 +22,7 @@ VALIDATE () {
         echo -e "$R $2 ... FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e "$R $2... SUCCESS $N" | tee -a $LOGS_FILE
+        echo -e "$G $2... SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
@@ -56,3 +57,15 @@ VALIDATE $? "Removing default data if exists"
 
 unzip /tmp/user.zip
 VALIDATE $? "Unzipping the code"
+
+npm install
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_NAME/users.service /etc/systemd/system/users.service
+VALIDATE $? "Creating systemctl services"
+
+systemctl daemon-reload
+
+systemctl enable user
+systemctl start user
+VALIDATE $? "Enabling and starting user"
